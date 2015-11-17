490aebedff560c46d009892ea25a090b6377d6d5
Aravind Srivatsan <aravindsrivats@hotmail.com>
2015-11-17 15:01:21 +0530

    :floppy_disk: Setup redis store

--- index.js
+++ index.js
@@ -3,16 +3,29 @@
 var express = require('express'),
     kraken = require('kraken-js'),
     debug = require('debug')('trifler'),
-    options, app, port;
+    redis = require('redis'),
+    redisHost = '127.0.0.1',
+    redisPort = 6379,
+    options, app, port, redisClient;
 
 options = {
     onconfig: function(config, next) {
-        next(null, config);
+      next(null, config);
     }
 };
 
 app = module.exports = express();
 app.use(kraken(options));
+
+redisClient = redis.createClient(redisPort, redisHost);
+redisClient.on('connect', function() {
+  debug('Redis Store connection established.');
+});
+
+app.use(function(req, res, next) {
+    req.redis = redisClient;
+    next();
+});
 
 app.on('start', function() {
     port = app.kraken.get('port');
