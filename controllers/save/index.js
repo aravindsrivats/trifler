bdd6472e8f0887cffdc34f06e6c4df57dd19e943
Aravind Srivatsan <aravindsrivats@hotmail.com>
2015-11-18 13:12:29 +0530

    :nail_care: Better minify for html, css and js

--- controllers/save/index.js
+++ controllers/save/index.js
@@ -2,7 +2,9 @@
 
 var shortid = require('shortid'),
     debug = require('debug')('trifler'),
-    minify = require('minify');
+    htmlminify = require('html-minifier').minify,
+    cleancss = require('clean-css'),
+    uglifyjs = require('uglify-js').minify;
 
 module.exports = function(router) {
     router.post('/', function(req, res) {
@@ -14,38 +16,24 @@ module.exports = function(router) {
         if (typeof req.body.html === 'undefined' || req.body.html === '') {
             trifle.html = '';
         } else {
-            minify.html(req.body.html, function(error, minified) {
-                if (error) {
-                    debug('HTML Minify error!');
-                } else {
-                    debug('Minifed HTML: ' + minified);
-                    trifle.html = minified;
-                }
+            trifle.html = htmlminify(req.body.html, {
+                keepClosingSlash: true
             });
+            debug('Minifed HTML: ' + trifle.html);
         }
         if (typeof req.body.css === 'undefined' || req.body.css === '') {
             trifle.css = '';
         } else {
-            minify.css(req.body.css, function(error, minified) {
-                if (error) {
-                    debug('CSS Minify error!');
-                } else {
-                    debug('Minifed CSS: ' + minified);
-                    trifle.css = minified;
-                }
-            });
+            trifle.css = new cleancss().minify(req.body.css).styles;;
+            debug('Minified CSS: ' + trifle.css);
         }
         if (typeof req.body.js === 'undefined' || req.body.js === '') {
             trifle.js = '';
         } else {
-            minify.js(req.body.js, function(error, minified) {
-                if (error) {
-                    debug('JS Minify error!');
-                } else {
-                    debug('Minified JS: ' + minified);
-                    trifle.js = minified;
-                }
-            });
+            trifle.js = uglifyjs(req.body.js, {
+                fromString: true
+            }).code;
+            debug('Minified JS: ' + trifle.js);
         }
         req.store.set(key, trifle, function(error) {
             if (error) {
